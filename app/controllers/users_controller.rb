a7e63e7c6b8fefc6c75c5fe3e8eaa22f975732d4
Ho-Ki Au <hau@scilearn.com.cn>
2013-08-14 16:51:39 +0800

    Add user microposts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,7 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find params[:id]
-    render 'show'
+    @microposts = @user.microposts.paginate page: params[:page]
   end
 
   def new
@@ -56,13 +56,6 @@ class UsersController < ApplicationController
   end
 
     # Before filters
-
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
 
     def correct_user
       @user = User.find(params[:id])
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessor :address
+  has_many :microposts, dependent: :destroy
 
   validates :name, presence: true
   validates_length_of :name, maximum: 50
@@ -13,6 +14,11 @@ class User < ActiveRecord::Base
 
   before_save { self.email = email.downcase }
   before_create :create_remember_token
+
+  def feed
+    # This is preliminary. See "Following users" for the full implementation.
+    Micropost.where("user_id = ?", id)
+  end
 
   def my_addy
     puts "I live in #{@address}"
